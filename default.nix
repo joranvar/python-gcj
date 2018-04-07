86b11bec9165790e17b0728a0b4d246eaef291b4
joranvar <bart.post@gmail.com>
2018-04-07 11:27:54 +0200

    Run solver on a given input

--- default.nix
+++ default.nix
@@ -1,19 +1,29 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-  for = problem : pkgs.haskellPackages.callCabal2nix (baseNameOf problem) (pkgs.stdenv.mkDerivation rec {
-  name = "${baseNameOf problem}-src";
+  for = solver : pkgs.haskellPackages.callCabal2nix (baseNameOf solver) (pkgs.stdenv.mkDerivation rec {
+  name = "${baseNameOf solver}-src";
   cabal = ./codejam.cabal;
   srcs = [ ./LICENSE ];
   phases = ["unpackPhase"];
   unpackPhase = ''
     set -Eux
     mkdir $out
-    cat ${cabal} | sed -e 's/Main.hs/${baseNameOf problem}/' > $out/codejam.cabal
-    cp ${problem} $out/${baseNameOf problem}
+    cat ${cabal} | sed -e 's/Main.hs/${baseNameOf solver}/' > $out/codejam.cabal
+    cp ${solver} $out/${baseNameOf solver}
     ${pkgs.lib.concatStrings (map (f: "cp ${f} $out/${baseNameOf f}") srcs)}
     set +Eux
   '';
-}) {};
+  }) {};
+  run = solver : input : pkgs.stdenv.mkDerivation {
+  name = "${baseNameOf solver}-on-${baseNameOf input}";
+  src = input;
+  phases = ["buildPhase"];
+  buildPhase = ''
+    set -Eux
+    ${for solver}/bin/codejam < ${input} | tee $out
+    set +Eux
+  '';
+  };
 in {
-  slarbo = for ./slarbo.hs;
+  slarbo = run ./slarbo.hs /home/joranvar/B-large-practice.in;
 }
